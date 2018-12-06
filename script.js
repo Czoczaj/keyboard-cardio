d0eb114a2c0c1833e0b2c6b7751560dcc94e0f14
ugandandyjczyk <mikolajdomanski7@gmail.com>
2018-12-06 18:18:15 +0100

    zad dom gotowe

--- script.js
+++ script.js
@@ -1,16 +1,4 @@
 var losNum = function(min,max){
-    var minimal = min;
-    var maximal = max;
-    console.log(minimal);
-    if(min<max){
-        var liczba = Math.floor(Math.random()*max);
-        if(liczba>min){
-            return(liczba);
-        }else{
-            losNum(minimal,maximal);
-        }
-    }else{
-        return(10000);
-    }
+ return( Math.floor(Math.random() * (max - (min -1) + min)));
 }
 console.log(losNum(3,10));
