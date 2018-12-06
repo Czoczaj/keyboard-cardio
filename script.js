38a5ce160c845c71c645c8cc328dfd41a9987b67
ugandandyjczyk <mikolajdomanski7@gmail.com>
2018-12-06 18:31:36 +0100

    some keyboard fixes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 var losNum = function(min,max){
- return( Math.floor(Math.random() * (max - (min -1) + min)));
+ return( Math.floor(Math.random() * (max - (min + 1) + min)));
 }
 console.log(losNum(3,10));
