2ef31f8cd0ff205f2767765251e5600f5bd8971d
ugandandyjczyk <mikolajdomanski7@gmail.com>
2018-12-06 19:46:56 +0100

    random key fire

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
+const lista = document.querySelectorAll('[data-key]');
+let lastKey;
+
 var losNum = function(min,max){
- return( Math.floor(Math.random() * (max - (min + 1) + min)));
+ return( Math.round(Math.random() * (max - min)) + min);
 }
-console.log(losNum(3,10));
+
+var losKey = function(){
+    var index = Math.floor(Math.random() * lista.length);
+    var key = lista[index];
+    if (key === lastKey) {
+        return losKey;
+    }
+    lastKey = key;
+    return key
+}
+var fireKey = function(){
+    var key = losKey();
+    key.classList.add('fire-key');
+    console.log(key.classList);
+    
+};
+fireKey();
