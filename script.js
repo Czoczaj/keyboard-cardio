cd89958a762e9029f202eadcb674271e4a5fd9bc
ugandandyjczyk <4321nitro1234@gmail.com>
2018-12-13 19:55:52 +0100

    a lot of things

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const buttonsKey = document.getElementsByClassName('key');
 let lastKey;
+let offGame = true;
+let currentKey;
+let score = 0;
 
 function getRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
@@ -18,3 +21,40 @@ function getRandomKey() {
 
 
 console.log(getRandomKey());
+function peep(){
+    lastKey.classList.remove('fire-key');
+    const time = getRandomNumber(1000,2000);
+    const key = getRandomKey();
+    currentKey = key;
+    key.classList.add('fire-key');
+    //setTimeout(peep, time);
+    setTimeout(() => {
+        if(!offGame)
+        {peep()}}, time);
+}
+function endGame(){
+    offGame = true;
+    peep();
+}
+function startGame(){
+    window.addEventListener('keydown',checkKey);
+    offGame = false;
+    peep();
+}
+function addClass(key,klasa){
+    key.classList.add(klasa);
+    setTimeout(() => {key.classList.remove(klasa),500})
+}
+function checkKey(event){
+    console.log(event.keyCode);
+    currentKey.classList.remove('fire-key');
+    if(currentKey.attributes[0].value == event.keyCode){ 
+        console.log(score);
+        addClass(currentKey,'correct-key');
+        score ++;
+    }else {
+        score --;
+        addClass(currentKey,'wrong-key');
+    }
+}
+peep();
